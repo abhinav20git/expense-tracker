08315318644c991421cd9c9d524b564e3e00d4bb
abhinav20git <118235094+abhinav20git@users.noreply.github.com>
2024-08-01 23:55:37 +0530

    df

---
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { UserSettings } from "@prisma/client"
+
+interface Props{
+    userSettings:UserSettings;
+    from:Date;
+    to:Date;
+}
+function CategoriesStats({userSettings,from,to}:Props) {
+  return (
+    <div></div>
+  )
+}
+
+export default CategoriesStats
